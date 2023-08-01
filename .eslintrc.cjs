a1a688288c1bf2b9b1352bbabd4df69c1902fb49
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-01 15:23:52 +0300

    feat: RSS-ECOMM-1_12

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json'
