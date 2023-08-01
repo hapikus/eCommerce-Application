6a9706fcf35f7fd307fe8511fc6ff951de47c26d
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-01 14:47:00 +0300

    feat: RSS-ECOMM-1_10, RSS-ECOMM-1_11

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
