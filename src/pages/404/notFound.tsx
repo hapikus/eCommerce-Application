5aebff55723d9ff3d48c8ab6ce7d32a8b9458448
Lukshaolya <lukshaolya98@gmail.com>
2023-08-11 21:25:25 +0300

    fix: rename Login folder

--- src/pages/404/notFound.tsx
+++ src/pages/404/notFound.tsx
@@ -9,7 +9,9 @@ function NotFound() {
       <div className={styles.infoBlock}>
         <h2 className={styles.title404}>404</h2>
         <h3 className={styles.title404}>Not found</h3>
-        <Button><Link to="/">To main page</Link></Button>
+        <Button>
+          <Link to="/">To main page</Link>
+        </Button>
       </div>
       <div className={styles.card} />
     </div>
--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './Main/main';
-import Login from './Login/login';
+import Login from './LoginPageExample/login';
 import NotFound from './404/notFound';
 import SignUp from './SignUp/signup';
 
