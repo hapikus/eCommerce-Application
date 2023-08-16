e437f4fd057d0bb0e5bf6714e1d274bb1dcf8fe3
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-16 14:35:13 +0300

    feat: adapt login page for mobile

--- src/utils/passValid.ts
+++ src/utils/passValid.ts
@@ -7,19 +7,35 @@ const passwordValidationRules = [
         return Promise.reject(new Error('Please input your password!'));
       }
       if (value.length < 8) {
-        return Promise.reject(new Error('Password must be at least 8 characters long.'));
+        return Promise.reject(
+          new Error('Password must be at least 8 characters long.'),
+        );
       }
       if (!/[A-Z]/.test(value)) {
-        return Promise.reject(new Error('Password must contain at least one uppercase letter (A-Z).'));
+        return Promise.reject(
+          new Error(
+            'Password must contain at least one uppercase letter (A-Z).',
+          ),
+        );
       }
       if (!/[a-z]/.test(value)) {
-        return Promise.reject(new Error('Password must contain at least one lowercase letter (a-z).'));
+        return Promise.reject(
+          new Error(
+            'Password must contain at least one lowercase letter (a-z).',
+          ),
+        );
       }
       if (!/\d/.test(value)) {
-        return Promise.reject(new Error('Password must contain at least one digit (0-9).'));
+        return Promise.reject(
+          new Error('Password must contain at least one digit (0-9).'),
+        );
       }
       if (value.trim() !== value) {
-        return Promise.reject(new Error('Password must not contain leading or trailing whitespace.'));
+        return Promise.reject(
+          new Error(
+            'Password must not contain leading or trailing whitespace.',
+          ),
+        );
       }
       return Promise.resolve();
     },
