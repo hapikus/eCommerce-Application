c212df29a7fa676bed783a87adc3289a6b6f4e30
Sergey Chikunov <84094895+hapikus@users.noreply.github.com>
2023-08-21 14:26:14 +0300

    feat: update vite.config for build rep

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/super-store-s2/',
   plugins: [react()],
-})
+});
