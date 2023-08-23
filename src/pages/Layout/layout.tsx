f42f9d320a50d93d998dcf796a97e5f1393cdc76
SwallowOnes <jager.selion@gmail.com>
2023-08-23 12:47:42 +0500

    fix: fix layout

--- src/pages/Layout/layout.tsx
+++ src/pages/Layout/layout.tsx
@@ -1,7 +1,7 @@
 import { Layout, Space } from 'antd';
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
 import styles from './layout.module.css';
 
 function LayoutPage() {
