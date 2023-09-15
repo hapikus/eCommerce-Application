59319380d3aea5daf883afbdc01a445ab11c1e14
Lukshaolya <lukshaolya98@gmail.com>
2023-09-15 20:47:00 +0300

    feat: about us section 3

--- src/pages/Info/info.tsx
+++ src/pages/Info/info.tsx
@@ -1,5 +1,80 @@
+import {
+  CommentOutlined,
+  LaptopOutlined,
+  ScheduleOutlined,
+  SoundOutlined,
+} from '@ant-design/icons';
+
+import styles from './info.module.css';
+
 function InfoPage() {
-  return <h1>Information</h1>;
+  return (
+    <div className={styles.pageContainer}>
+      <section className={styles.sectionContaier}>
+        <div className={styles.infoMainContaier}>
+          <div className={styles.teamGreet}>
+            <h3>
+              Its code<span>Frondlers</span>Team
+            </h3>
+          </div>
+        </div>
+      </section>
+      <section className={styles.aboutTeamContainer}>
+        <div className={styles.sectionContaier}>
+          <div className={styles.sectionTitle}>
+            <h2> How we work </h2>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.columnCapabilitis}>
+              <div className={styles.icon}>
+                <CommentOutlined />
+              </div>
+              <h3> Effective communication </h3>
+              <p>
+                Understand the importance of clear and timely communication for
+                successful project development.
+              </p>
+            </div>
+            <div className={styles.columnCapabilitis}>
+              <div className={styles.icon}>
+                <ScheduleOutlined />
+              </div>
+              <h3> Synchronized work </h3>
+              <p>
+                Collaborate effectively, sharing ideas and knowledge to achieve a
+                common goal. Delegate tasks. Achieve success together.
+              </p>
+            </div>
+            <div className={styles.columnCapabilitis}>
+              <div className={styles.icon}>
+                <LaptopOutlined />
+              </div>
+              <h3> Modern Web Development </h3>
+              <p>
+                Listen to the needs and expectations in order to create a unique
+                online space for the store.
+              </p>
+            </div>
+            <div className={styles.columnCapabilitis}>
+              <div className={styles.icon}>
+                <SoundOutlined />
+              </div>
+              <h3> Flexibility and adaptability</h3>
+              <p>
+                Ready to adapt to changes and fix errors on the go. Quickly
+                respond to feedback and offer alternative solutions.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={styles.sectionContaier}>
+        <div className={styles.infoMainContaier}>
+          <h2>dgf</h2>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default InfoPage;
